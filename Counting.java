4fd4fd48d8a1be9d2147ea969e13270db96c6b19
xren11 <xianliren@gmail.com>
2016-03-03 02:44:14 -0600

    upload Counting.java

--- Counting.java
+++ Counting.java
@@ -14,29 +14,46 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class Counting {
 
-	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-		private final static IntWritable one = new IntWritable(1);
+	public static class Map extends Mapper<LongWritable, Text, Text, Text> {
+		//private final static IntWritable one = new IntWritable(1);
 		private Text word = new Text();
+		private Text collection = new Text();
 
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-			String line = value.toString();
-			StringTokenizer tokenizer = new StringTokenizer(line);
-			while (tokenizer.hasMoreTokens()) {
-				word.set(tokenizer.nextToken());
-				context.write(word, one);
-			}
+			//String line = value.toString();
+			String[] content = value.toString().split(",");
+			String info = content[2] + "," + content[3] + "," + content[4];
+			word.set(content[5]);
+			collection.set(info);
+			context.write(word, collection);
+			// StringTokenizer tokenizer = new StringTokenizer(line);
+			// while (tokenizer.hasMoreTokens()) {
+			// 	word.set(tokenizer.nextToken());
+			// 	context.write(word, one);
+			// }
 		}
 	} 
 
-	public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+	public static class Reduce extends Reducer<Text, Text, Text, Text> {
 
-		public void reduce(Text key, Iterable<IntWritable> values, Context context) 
+		public void reduce(Text key, Iterable<Text> values, Context context) 
 		throws IOException, InterruptedException {
-			int sum = 0;
-			for (IntWritable val : values) {
-				sum += val.get();
+			int cnt1 = 0, cnt2 = 0, cnt3 = 0;
+			for (Text val: values) {
+				String[] content = val.toString().split(",");
+                if (content[0].equals("1")) ++cnt1;
+                if (content[1].equals("1")) ++cnt2;
+                if (content[2].equals("1")) ++cnt3;
 			}
-			context.write(key, new IntWritable(sum));
+			String info = "," + cnt1 + "," + cnt2 + "," + cnt3;
+			Text result = new Text()
+			result.set(info);
+			context.write(key, result);
+			// int sum = 0;
+			// for (IntWritable val : values) {
+			// 	sum += val.get();
+			// }
+			// context.write(key, new IntWritable(sum));
 		}
 	}
 
@@ -50,7 +67,8 @@ public class Counting {
 
 
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
+		job.setOutputValueClass(Text.class);
+		//job.setOutputValueClass(IntWritable.class);
 
 		job.setMapperClass(Map.class);
 		job.setReducerClass(Reduce.class);
