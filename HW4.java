1b189f1b2b84c339be2d40d3f78582d7c07835d6
xren11 <xianliren@gmail.com>
2016-03-04 00:03:03 -0600

    hw4

--- HW4.java
+++ HW4.java
@@ -79,60 +79,60 @@ public class HW4 {
 	  	ArrayList<String> AList = new ArrayList<String>() ;
 		ArrayList<String> BList = new ArrayList<String>() ;
 		Text nullText = new Text();
-		for (Text val: values) {
-			context.write(nullText,val);
-		}
+		// for (Text val: values) {
+		// 	context.write(nullText,val);
+		// }
 		//while (values.hasNext()) {
-// 		for (Text val: values) {
-// 			String content = val.toString();
-// 			//String content = values.next().toString();
-// 			if(content.charAt(0) == 'A') AList.add(content);
-// 			else BList.add(content);
-// 		}
-// 		int ASize = AList.size();
-// 		int BSize = BList.size();
-// 		String result = new String();
-// 		for(int i=0;i<ASize; i++){
-// 			String contentA = AList.get(i);
-// 			String [] numberA = contentA.split(",");
-// 			String timeA = numberA[1];
-// 			String userA = numberA[2];
-// 			String queryA = numberA[3];
-// 			Date dateA =  new Date();//new SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
-// 			Date dateB =  new Date();//SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
-// 			try{
-// 				dateA = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(timeA);
-// 			}
-// 			catch(ParseException pe){
-// 				System.out.println("ERROR: could not parse date in string \"" + timeA + "\"");
-// 			}
-// 			for(int j=0;j<BSize; j++){
-// 				String contentB = BList.get(j);
-// 				String [] numberB = contentB.split(",");
-// 				String timeB = numberB[1];
-// 				String userB = numberB[2];
-// 				String queryB = numberB[3];
-// 				if(!userA.equals(userB) ){
-// 					try{
-// 						dateB = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(timeB);
-// 					}
-// 					catch(ParseException pe){
-// 						System.out.println("ERROR: could not parse date in string \"" + timeA + "\"");
-// 					}
-// //					Date dateB = new SimpleDateFormat("YYYY-MM-DD HH:mm:ss").parse(timeB);
-// 					long timeDiff = Math.abs(dateA.getTime() - dateB.getTime())/1000;
-// 					if(timeDiff < 2 ){
-// //						result += timeA + "," + timeB + "," + queryA +"," + queryB + "\n";
-// 						result = timeA + "," + userA +"," + userB;
-// 						//result = timeA + "," + queryA +"," + queryB;
-// 						Text record = new Text(result);
-// 						//output.collect(nullText,record);
+		for (Text val: values) {
+			String content = val.toString();
+			//String content = values.next().toString();
+			if(content.charAt(0) == 'A') AList.add(content);
+			else BList.add(content);
+		}
+		int ASize = AList.size();
+		int BSize = BList.size();
+		String result = new String();
+		for(int i=0;i<ASize; i++){
+			String contentA = AList.get(i);
+			String [] numberA = contentA.split(",");
+			String timeA = numberA[1];
+			String userA = numberA[2];
+			String queryA = numberA[3];
+			Date dateA =  new Date();//new SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
+			Date dateB =  new Date();//SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
+			try{
+				dateA = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(timeA);
+			}
+			catch(ParseException pe){
+				System.out.println("ERROR: could not parse date in string \"" + timeA + "\"");
+			}
+			for(int j=0;j<BSize; j++){
+				String contentB = BList.get(j);
+				String [] numberB = contentB.split(",");
+				String timeB = numberB[1];
+				String userB = numberB[2];
+				String queryB = numberB[3];
+				if(!userA.equals(userB) ){
+					try{
+						dateB = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(timeB);
+					}
+					catch(ParseException pe){
+						System.out.println("ERROR: could not parse date in string \"" + timeA + "\"");
+					}
+//					Date dateB = new SimpleDateFormat("YYYY-MM-DD HH:mm:ss").parse(timeB);
+					long timeDiff = Math.abs(dateA.getTime() - dateB.getTime())/1000;
+					if(timeDiff < 2 ){
+//						result += timeA + "," + timeB + "," + queryA +"," + queryB + "\n";
+						result = timeA + "," + userA +"," + userB;
+						//result = timeA + "," + queryA +"," + queryB;
+						Text record = new Text(result);
+						//output.collect(nullText,record);
 
-// 						context.write(nullText, record);
-// 					}
-// 				}
-// 			}
-// 		}
+						context.write(nullText, record);
+					}
+				}
+			}
+		}
 
 //		Text record = new Text(result);
 //		output.collect(key, record);
