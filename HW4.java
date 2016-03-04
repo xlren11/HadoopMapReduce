4264346b88334132731d2514b6b9470057560953
xren11 <xianliren@gmail.com>
2016-03-04 01:32:57 -0600

    hw4

--- HW4.java
+++ HW4.java
@@ -11,112 +11,59 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.io.*;
-//import org.apache.hadoop.mapred.*;
-//import org.apache.hadoop.util.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
-// Build a 100 times 100 matrix
-// There are 10000 reducers, enough for HW4
 
 public class HW4 {
 	public static class Map extends Mapper<LongWritable, Text, Text, Text> {
-//	  private Text region = new Text();
-//	  private Text contentA = new Text();
-//	  private Text contentB = new Text();
-//	  private int dimension = 25;
-//	  Random generater = new Random();
-	  public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {		
-	  //public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-	 //  	Text region = new Text();
-		// Text contentA = new Text();
-		// Text contentB = new Text();
-		//int dimension = 25;
-		//String line = value.toString();
-		String [] words = value.toString().split(",");
-		//String clicks = words[3];
-	  	//Random generater = new Random();
-		if(words[3].equals( "1") && !words[1].isEmpty()){
-			String dataA = "A," + words[0] + "," + words[1];
-			String dataB = "B," + words[0] + "," + words[1];
-			context.write(new Text("AAA"), new Text(dataA));
-			context.write(new Text("AAA"), new Text(dataB));
-		}
-	  }
+	    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {		
+			String [] words = value.toString().split(",");
+			if(words[3].equals( "1") && !words[1].isEmpty()){
+				String dataA = "A," + words[0] + "," + words[1];
+				String dataB = "B," + words[0] + "," + words[1];
+				context.write(new Text("AAA"), new Text(dataA));
+				context.write(new Text("AAA"), new Text(dataB));
+			}
+	  	}
 	}
 
 	public static class Reduce extends Reducer<Text, Text, Text, Text> {
-//	  private ArrayList<String> AList = new ArrayList<String>() ;
-//	  private ArrayList<String> BList = new ArrayList<String>() ;
-//	  @override
-	  public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-	  	ArrayList<String> listA = new ArrayList<String>() ;
-		ArrayList<String> listB = new ArrayList<String>() ;
-		//Text nullText = new Text();
-		// for (Text val: values) {
-		// 	String[] ss = val.toString().split(",");
-		// 	context.write(nullText,new Text(ss[2]));
-		// }
-		//while (values.hasNext()) {
-		for (Text val: values) {
-			String content = val.toString();
-			//String content = values.next().toString();
-			if(content.charAt(0) == 'A') listA.add(content);
-			else listB.add(content);
-		}
-		//int ASize = AList.size();
-		//int BSize = BList.size();
-		//String result = new String();
-		//for(int i=0;i<ASize; i++){
-		for (String strA: listA) {
-			for (String strB: listB) {
-			//String contentA = AList.get(i);
-				String [] dataA = strA.split(",");
-				String [] dataB = strB.split(",");
-				// String ymdhA = dataA[1];
-				// String timeB = dataB[1];
-				String idA = dataA[2];
-				String idB = dataB[2];
-				if(!idA.equals(idB)){
-				//String queryA = numberA[3];
-					Date dateA =  new Date();//new SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
-					Date dateB =  new Date();//SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
-					try{
-						dateA = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dataA[1]);
-					}
-					catch(ParseException pe){
-						//System.out.println("ERROR: could not parse date in string \"" + timeA + "\"");
-					}
-			//for(int j=0;j<BSize; j++){
-			//for (String strB: listB) {
-				//String contentB = BList.get(j);
 
-				//String queryB = numberB[3];
-				
-					try{
-						dateB = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dataB[1]);
-					}
-					catch(ParseException pe){
-						//System.out.println("ERROR: could not parse date in string \"" + timeA + "\"");
-					}
-//					Date dateB = new SimpleDateFormat("YYYY-MM-DD HH:mm:ss").parse(timeB);
-					long diff = Math.abs(dateA.getTime() - dateB.getTime()) / 1000;
-					if (diff < 2 ){
-//						result += timeA + "," + timeB + "," + queryA +"," + queryB + "\n";
-						String result = dataA[1] + "," + idA +"," + idB;
-						//result = timeA + "," + queryA +"," + queryB;
-						//Text record = new Text(result);
-						//output.collect(nullText,record);
-
-						context.write(new Text(), new Text(result));
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+	  		ArrayList<String> listA = new ArrayList<String>() ;
+			ArrayList<String> listB = new ArrayList<String>() ;
+			for (Text val: values) {
+				String content = val.toString();
+				if(content.charAt(0) == 'A') listA.add(content);
+				else listB.add(content);
+			}
+			for (String strA: listA) {
+				for (String strB: listB) {
+					String [] dataA = strA.split(",");
+					String [] dataB = strB.split(",");
+					String idA = dataA[2];
+					String idB = dataB[2];
+					if (!idA.equals(idB)){
+						Date dateA =  new Date();
+						Date dateB =  new Date();
+						try {
+							dateA = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dataA[1]);
+						}
+						catch(ParseException pe){}
+						try{ 
+							dateB = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dataB[1]);
+						}
+						catch(ParseException pe){}
+						long diff = Math.abs(dateA.getTime() - dateB.getTime()) / 1000;
+						if (diff < 2 ){
+							String result = dataA[1] + "," + idA +"," + idB;
+							context.write(new Text(), new Text(result));
+						}
 					}
 				}
 			}
 		}
-
-//		Text record = new Text(result);
-//		output.collect(key, record);
-	  }
 	}
 
 	public static void main(String[] args) throws Exception {
--- ThetaJoin.java
+++ ThetaJoin.java
@@ -2,158 +2,74 @@ package org.myorg;
 
 import java.io.IOException;
 import java.util.*;
-
+import java.util.Date;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
-import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.io.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
-import java.util.Date;
 
-
-public class ThetaJoin {
-
+public class Thetajoin {
 	public static class Map extends Mapper<LongWritable, Text, Text, Text> {
-
-		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-			// String[] content = value.toString().split(",");
-			// Text k = new Text();
-			// Text d1 = new Text();
-			// Text d2 = new Text();
-			// k.set("a");
-			// if (content[3].equals("1")) {
-			// 	String data1 = "x," + content[0] + "," + content[1];
-			// 	String data2 = "y," + content[0] + "," + content[1];
-			// 	d1.set(data1);
-			// 	d2.set(data2);
-			// 	context.write(k, d1);
-			// 	context.write(k, d2);
-			// }
-
-			Text region = new Text();
-			Text contentA = new Text();
-			Text contentB = new Text();
-			int dimension = 25;
-			String line = value.toString();
-			String [] words = line.split(",");
-			String clicks = words[3];
-			region.set("A");
-			if(clicks.equals( "1") ){
-				String messagesA = "A," + words[0] + "," + words[1];
-				String messagesB = "B," + words[0] + "," + words[1];
-				contentA.set(messagesA);
-				contentB.set(messagesB);
-				context.write(region,contentA);
-				context.write(region,contentB);
+	    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {		
+			String [] words = value.toString().split(",");
+			if(words[3].equals( "1") && !words[1].isEmpty()){
+				String dataA = "A," + words[0] + "," + words[1];
+				String dataB = "B," + words[0] + "," + words[1];
+				context.write(new Text("AAA"), new Text(dataA));
+				context.write(new Text("AAA"), new Text(dataB));
 			}
-		}
-	} 
+	  	}
+	}
 
 	public static class Reduce extends Reducer<Text, Text, Text, Text> {
 
-		public void reduce(Text key, Iterable<Text> values, Context context) 
-		throws IOException, InterruptedException{
-
-			// List<String> l1 = new ArrayList<String>();
-			// List<String> l2 = new ArrayList<String>();
-			// for (Text val: values) {
-			// 	String data = val.toString();
-			// 	if (data.charAt(0) == 'x') {
-			// 		l1.add(data);
-			// 	}
-			// 	else {
-			// 		l2.add(data);
-			// 	}
-			// }
-			// Text result = new Text();
-			// for (String s: l1) {
-			// 	for (String q: l2) {
-			// 		String[] d1 = s.split(",");
-			// 		String[] d2 = q.split(",");
-			// 		String id1 = d1[2];
-			// 		String id2 = d2[2];
-			// 		if (!id1.equals(id2)) {
-			// 			Date date1 = new Date();
-			// 			Date date2 = new Date();
-			// 			try {
-			// 				date1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(d1[1]);
-			// 			}
-			// 			catch(ParseException e) {}
-			// 			try {
-			// 				date2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(d2[1]);
-			// 			}
-			// 			catch(ParseException e) {}
-			// 			if (Math.abs(date1.getTime() - date2.getTime()) / 1000 < 2){
-			// 				String res = d1[1] + "," + d1[2] + "," + d2[2] + ",";
-			// 				result.set(res);
-			// 				context.write(result, new Text());
-			// 			}
-			// 		}
-			// 	}
-			// }		
-
-			ArrayList<String> AList = new ArrayList<String>() ;
-			ArrayList<String> BList = new ArrayList<String>() ;
-			Text nullText = new Text();
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+	  		ArrayList<String> listA = new ArrayList<String>() ;
+			ArrayList<String> listB = new ArrayList<String>() ;
 			for (Text val: values) {
 				String content = val.toString();
-				if(content.charAt(0) == 'A') AList.add(content);
-				else BList.add(content);
+				if(content.charAt(0) == 'A') listA.add(content);
+				else listB.add(content);
 			}
-			int ASize = AList.size();
-			int BSize = BList.size();
-			String result = new String();
-			for(int i=0;i<ASize; i++){
-				String contentA = AList.get(i);
-				String [] numberA = contentA.split(",");
-				String timeA = numberA[1].split(":")[2];
-				String userA = numberA[2];
-				Date dateA =  new Date();//new SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
-			    Date dateB =  new Date();//SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
-			    try{
-					dateA = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(timeA);
-				}
-				catch(ParseException pe){
-					System.out.println("ERROR: could not parse date in string \"" + timeA + "\"");
-				}
-				for(int j=0;j<BSize; j++){
-					String contentB = BList.get(j);
-					String [] numberB = contentB.split(",");
-					String timeB = numberB[1];
-					String userB = numberB[2];
-					if(!userA.equals(userB) ){
-						try{
-							dateB = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(timeB);
+			for (String strA: listA) {
+				for (String strB: listB) {
+					String [] dataA = strA.split(",");
+					String [] dataB = strB.split(",");
+					String idA = dataA[2];
+					String idB = dataB[2];
+					if (!idA.equals(idB)){
+						Date dateA =  new Date();
+						Date dateB =  new Date();
+						try {
+							dateA = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dataA[1]);
 						}
-						catch(ParseException pe){
-							System.out.println("ERROR: could not parse date in string \"" + timeA + "\"");
+						catch(ParseException pe){}
+						try{ 
+							dateB = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dataB[1]);
 						}
-						//Date dateB = new SimpleDateFormat("YYYY-MM-DD HH:mm:ss").parse(timeB);
-						long timeDiff = Math.abs(dateA.getTime() - dateB.getTime())/1000;
-						//long timeDiff = Math.abs(timaA - timb);
-						if(timeDiff < 2 ){
-	//						result += timeA + "," + timeB + "," + queryA +"," + queryB + "\n";
-							result += timeA + "," + userA +"," + userB + "\n";
-							Text record = new Text(result);
-							context.write(record, new Text());
+						catch(ParseException pe){}
+						long diff = Math.abs(dateA.getTime() - dateB.getTime()) / 1000;
+						if (diff < 2 ){
+							String result = dataA[1] + "," + idA +"," + idB;
+							context.write(new Text(), new Text(result));
 						}
 					}
 				}
-			}	
+			}
 		}
 	}
 
 	public static void main(String[] args) throws Exception {
-
 		Job job = new Job(new Configuration());
 
-		job.setJarByClass(ThetaJoin.class);
+		job.setJarByClass(Thetajoin.class);
 
 
 		job.setOutputKeyClass(Text.class);
@@ -170,5 +86,4 @@ public class ThetaJoin {
 
 		job.waitForCompletion(true);
 	}
-
 }
